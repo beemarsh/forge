5719580f9ce33337868e8ceba2b93e4068e507a4
beemarsh <anup8eguy@gmail.com>
2025-06-13 13:50:04 -0400

    NCCL Logs Disabled, Config Files Changed, Arguments Added to neox-env.sh

--- neox-env.sh
+++ neox-env.sh
@@ -1,15 +1,23 @@
+#!/bin/bash
+
 module load PrgEnv-gnu
 module load gcc/10.3.0
 module load rocm/5.3.0
 module load miniforge3
 
+env_path=""
+for arg in "$@"; do
+  if [[ $arg == --env-path=* ]]; then
+    env_path="${arg#*=}"
+    break
+  fi
+done
 
-export HCC_AMDGPU_TARGET=gfx90a
-export PYTORCH_ROCM_ARCH=gfx90a
+if [ -n "$env_path" ]; then
+  source activate "$env_path"
+else
+  echo "Error: The --env-path=<path> argument is required." >&2
+  return 1
+fi
 
-export LD_LIBRARY_PATH=/opt/rocm-$rocm_version/hip/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/lustre/orion/gen243/proj-shared/bbdir/forge/aws-ofi-rccl/lib:$LD_LIBRARY_PATH
-
-source activate /lustre/orion/gen243/proj-shared/bbdir/forge/conda-env-forge
-#conda config --append envs_dirs /lustre/orion/gen243/proj-shared/bbdir/forge/conda-env-forge
-#source activate conda-env-forge
+#source activate /lustre/orion/gen243/proj-shared/bbdir/forge/conda-env-forge
